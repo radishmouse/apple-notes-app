66385b652aea1f23ac66f066580a3c46552da957
Chris Aquino <chrisaquino@radishmouse.local>
2020-02-05 13:34:56 -0500

    makes SearchBar a controlled component

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -2,17 +2,38 @@ import React from 'react';
 import NewNote from './NewNote';
 import NotesList from './NotesList';
 import NoteEditor from './NoteEditor';
+import SearchBar from './SearchBar';
 
 class NotesApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: 'help me i am a search bar'
+        };
+    }
+
     render() {
         return (
             <div>
-                <h1>Best Notes App Ever</h1>
+                <h1>Notes App</h1>
                 <NewNote />
+                <SearchBar 
+                    handleChange={this._setSearchText}
+                    text={this.state.searchText} 
+                />
                 <NotesList />
                 <NoteEditor />
             </div>
         );
+    }
+
+    _setSearchText = (searchText) => {
+     this.setState({
+        searchText
+     }, () => {
+        console.log('updated search text');
+     });   
     }
 }
 
