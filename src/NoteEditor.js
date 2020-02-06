e1d309523faabd89ba2d519eeedaa7da1049b998
Chris Aquino <chrisaquino@radishmouse.local>
2020-02-06 10:19:39 -0500

    changes NoteEditor to a class

--- src/NoteEditor.js
+++ src/NoteEditor.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
-function NoteEditor({
-    note,
-    handleChange
-}) {
+class NoteEditor extends React.Component {
 
-    return (
-        <div>
-            <input 
-                value={note.title} 
-                onChange={(event) => {
-                    // NOOOOOOOOO!
-                    // Don't mutate an object or array
-                    // that you receive as an argument.
-                    // note.title = event.target.value;  
-                    
-                    // Instead, send back a copy
-                    // with the updated "title" 
-                    handleChange({
-                        ...note,
-                        title: event.target.value
-                    });
-                }}
-            />
-            <br />
-            <textarea 
-                value={note.copy} 
-                onChange={(event) => {
-                    handleChange({
-                        ...note,
-                        copy: event.target.value
-                    });
-                }}
-            />
-        </div>
-    );
+    
+    render() {
+        const {
+            note,
+            handleChange
+        } = this.props;
+
+        return (
+            <div>
+                <input 
+                    value={note.title} 
+                    onChange={(event) => {
+                        // NOOOOOOOOO!
+                        // Don't mutate an object or array
+                        // that you receive as an argument.
+                        // note.title = event.target.value;  
+                        
+                        // Instead, send back a copy
+                        // with the updated "title" 
+                        handleChange({
+                            ...note,
+                            title: event.target.value
+                        });
+                    }}
+                />
+                <br />
+                <textarea 
+                    value={note.copy} 
+                    onChange={(event) => {
+                        handleChange({
+                            ...note,
+                            copy: event.target.value
+                        });
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 export default NoteEditor;
